b596525ac0d4498b788bd99926fa26ac0ae99061
He Xu <x.he@jacobs-university.de>
2016-03-29 00:34:09 +0200

    Change Format and Test Runtime

--- Pipeline/AudioPipe/speaker/SpeakerID.py
+++ Pipeline/AudioPipe/speaker/SpeakerID.py
@@ -5,6 +5,7 @@ from silence import remove_silence
 import scipy.io.wavfile as wav
 from time import localtime, strftime
 import sys, getopt, os
+import datetime
 
 def readSegFile(segfn):
     inFile = open(segfn)
@@ -16,8 +17,8 @@ def readSegFile(segfn):
         segments.append(cols)
     return segments
 
-def myLengend(speakermodel):
-    lengend = 'SPK_01|'+ strftime("%Y-%m-%d %H:%M", localtime()) + '|Source_Program=SpeakerID.py ' + \
+def myLengend(speakermodel,primary):
+    lengend = primary+'|'+ strftime("%Y-%m-%d %H:%M", localtime()) + '|Source_Program=SpeakerID.py ' + \
     speakermodel + '|Source_Person=He Xu\n'
     return lengend
 
@@ -31,12 +32,16 @@ def readHeader(infofn):
     fileText = inFile.read()
     lines = fileText.split('\n')
     header = ''
+    TOP = None
     for line in lines:
         header = header + line + '\n'
+        if line.startswith('TOP') and TOP is None:
+            segs = line.split('|')
+            TOP = segs[1]
         if line.startswith('LBT'):
             break
     endline = lines[-2]
-    return header, endline
+    return header, endline, TOP
 
 def readSegFeat(start_t, end_t, signal, sr):
     try:
@@ -58,19 +63,36 @@ def main(argv):
     infofn = argv[3]
     dest = argv[4]
     ext = argv[5]
+    if "gen" in ext:
+        classnm = "Gender="
+    else:
+        classnm = "Name="
+        
+    primary_tag = ext[1:].upper()+"_01"
 
 
     segments = readSegFile(segfn)
     fn = infofn.split('.')[0].split('/')[-1]
     outFile = open(os.path.join(dest,fn+ext), 'w')
-    (header, endline) = readHeader(infofn)
+    (header, endline, TOP) = readHeader(infofn)
     outFile.write(header)
-    outFile.write(myLengend(speakermodel))
+    TOP_year = int(TOP[:4])
+    TOP_month = int(TOP[4:6])
+    TOP_day = int(TOP[6:8])
+    TOP_hour = int(TOP[8:10])
+    TOP_min = int(TOP[10:12])
+    TOP_sec = int(TOP[12:14])
+    t = datetime.time(TOP_hour, TOP_min, TOP_sec)
+    d = datetime.date(TOP_year, TOP_month, TOP_day)
+    dt = datetime.datetime.combine(d, t)
+    
+    outFile.write(myLengend(speakermodel,primary_tag))
     speakerRec = SR.GMMRec.load(speakermodel)
     (sr, signal) = wav.read(audiofn)
     if len(signal.shape) > 1:
-            signal = signal[:, 0]    
-    for i in xrange(len(segments)-1):
+            signal = signal[:, 0]  
+    tformat = "%02Y%02m%02d%02H%02M%02S"
+    for i in xrange(len(segments)-1):   
         start_time = float(segments[i][3])
         try:
             end_time = float(segments[i+1][3])
@@ -81,8 +103,12 @@ def main(argv):
         except:
             print (start_time, end_time)
         (speaker, llhd) = speakerRec.predict(segfeat)
-        entry = "%d:%02d:%02d|" % totime(start_time) + \
-        "%d:%02d:%02d|" % totime(end_time) + "Person=" + speaker + "|Log Likelihood=" + str(llhd) + "\n"
+        strdt_start = dt+datetime.timedelta(seconds=start_time)
+        timestr_start = "%s.%03d|" % (strdt_start.strftime(tformat),strdt_start.microsecond/1000)
+        strdt_end = dt+datetime.timedelta(seconds=end_time)
+        timestr_end = "%s.%03d|" % (strdt_end.strftime(tformat),strdt_end.microsecond/1000)
+        entry = timestr_start + \
+        timestr_end + primary_tag+'|'+classnm + speaker + "|Log Likelihood=" + str(llhd) + "\n"
         outFile.write(entry)
     outFile.write(endline)
     outFile.close()
--- Pipeline/AudioPipe/speaker/SpeakerRec.py
+++ Pipeline/AudioPipe/speaker/SpeakerRec.py
@@ -6,6 +6,8 @@ import scipy.io.wavfile as wav
 from time import localtime, strftime
 import sys, getopt, os
 import numpy as np
+import datetime
+
 
 def readSegFile(segfn):
     inFile = open(segfn)
@@ -17,8 +19,8 @@ def readSegFile(segfn):
         segments.append(cols)
     return segments
 
-def myLengend(speakermodel):
-    lengend = 'SPK_01|'+ strftime("%Y-%m-%d %H:%M", localtime()) + '|Source_Program=SpeakerRec.py ' + \
+def myLengend(speakermodel,primary):
+    lengend = primary+'|'+ strftime("%Y-%m-%d %H:%M", localtime()) + '|Source_Program=SpeakerRec.py ' + \
     speakermodel + '|Source_Person=He Xu\n'
     return lengend
 
@@ -32,12 +34,16 @@ def readHeader(infofn):
     fileText = inFile.read()
     lines = fileText.split('\n')
     header = ''
+    TOP = None
     for line in lines:
         header = header + line + '\n'
+        if line.startswith('TOP') and TOP is None:
+            segs = line.split('|')
+            TOP = segs[1]
         if line.startswith('LBT'):
             break
     endline = lines[-2]
-    return header, endline
+    return header, endline, TOP
 
 def readSegFeat(start_t, end_t, signal, sr):
     try:
@@ -58,12 +64,28 @@ def main(argv):
     infofn = argv[2]
     dest = argv[3]
     ext = argv[4]
+    if "gen" in ext:
+        classnm = "Gender="
+    else:
+        classnm = "Name="
+        
+    primary_tag = ext[1:].upper()+"_02"
         
     fn = infofn.split('.')[0].split('/')[-1]
     outFile = open(os.path.join(dest,fn+ext), 'w')
-    (header, endline) = readHeader(infofn)
+    (header, endline, TOP) = readHeader(infofn)
     outFile.write(header)
-    outFile.write(myLengend(speakermodel))
+    TOP_year = int(TOP[:4])
+    TOP_month = int(TOP[4:6])
+    TOP_day = int(TOP[6:8])
+    TOP_hour = int(TOP[8:10])
+    TOP_min = int(TOP[10:12])
+    TOP_sec = int(TOP[12:14])
+    t = datetime.time(TOP_hour, TOP_min, TOP_sec)
+    d = datetime.date(TOP_year, TOP_month, TOP_day)
+    dt = datetime.datetime.combine(d, t)
+    
+    outFile.write(myLengend(speakermodel,primary_tag))
     speakerRec = SR.GMMRec.load(speakermodel)
     (sr, signal) = wav.read(audiofn)
     start_time = 0
@@ -71,15 +93,20 @@ def main(argv):
     duration = 5
     totallen = np.round(signal.shape[0] / sr).astype(int)
     if len(signal.shape) > 1:
-            signal = signal[:, 0]   
+            signal = signal[:, 0]  
+    tformat = "%02Y%02m%02d%02H%02M%02S"
     while start_time < totallen:
         end_time = start_time + duration
         if end_time > totallen:
             end_time = totallen
         segfeat = readSegFeat(start_time, end_time, signal, sr)
         (speaker, llhd) = speakerRec.predict(segfeat)
-        entry = "%d:%02d:%02d|" % totime(start_time) + \
-        "%d:%02d:%02d|" % totime(end_time) + "Person=" + speaker + "|Log Likelihood=" + str(llhd) + "\n"
+        strdt_start = dt+datetime.timedelta(seconds=start_time)
+        timestr_start = "%s.%03d|" % (strdt_start.strftime(tformat),strdt_start.microsecond/1000)
+        strdt_end = dt+datetime.timedelta(seconds=end_time)
+        timestr_end = "%s.%03d|" % (strdt_end.strftime(tformat),strdt_end.microsecond/1000)
+        entry = timestr_start + \
+        timestr_end + primary_tag+'|'+classnm+ speaker + "|Log Likelihood=" + str(llhd) + "\n"
         outFile.write(entry)
         start_time += step
     outFile.write(endline)
